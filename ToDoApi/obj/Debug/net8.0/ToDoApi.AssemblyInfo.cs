62056012ce9e2f7849526a994fea64e136a939e6
Anthony Hildebrand Chendrik <anthony@anthony.anthony>
2024-11-13 01:23:43 -0400

    some changes

--- ToDoApi/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
+++ ToDoApi/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87094e55c543ad0ad2123e4dbfa6e2b6c5425550")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
